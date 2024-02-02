c53798f95a6ab43f88d3bfa8e25a20b3b2610e3e
Santiago <dellasala1992@gmail.com>
2024-02-02 00:08:57 -0300

    Primer commit

---
+++ src/controllers/moviesController.js
@@ -0,0 +1,93 @@
+const db = require('../database/models');
+const sequelize = db.sequelize;
+
+//Otra forma de llamar a los modelos
+const Movies = db.Movie;
+
+const moviesController = {
+    'list': (req, res) => {
+        db.Movie.findAll()
+            .then(movies => {
+                res.render('moviesList.ejs', {movies})
+            })
+    },
+    'detail': (req, res) => {
+        db.Movie.findByPk(req.params.id)
+            .then(movie => {
+                res.render('moviesDetail.ejs', {movie});
+            });
+    },
+    'new': (req, res) => {
+        db.Movie.findAll({
+            order : [
+                ['release_date', 'DESC']
+            ],
+            limit: 5
+        })
+            .then(movies => {
+                res.render('newestMovies', {movies});
+            });
+    },
+    'recomended': (req, res) => {
+        db.Movie.findAll({
+            where: {
+                rating: {[db.Sequelize.Op.gte] : 8}
+            },
+            order: [
+                ['rating', 'DESC']
+            ]
+        })
+            .then(movies => {
+                res.render('recommendedMovies.ejs', {movies});
+            });
+    }, //Aqui debemos modificar y completar lo necesario para trabajar con el CRUD
+    add: function (req, res) {
+        db.Genre.findAll({
+            order : [
+                ['name']
+            ]
+        })
+            .then(genres =>{
+                return res.render('moviesAdd', {
+                    genres
+                })
+            })
+            .catch(error => console.log(error));
+    },
+    create: function (req, res) {
+        const {title, release_date, awards, rating, length, genre_id} = req.body
+
+        if ([title, release_date, awards, rating, length, genre_id].includes("")) {
+            return res.render('moviesAdd',{
+                messageError : "Todos los campos son obligatorios"
+            })
+        }else{
+            db.Movie.create({
+                title : title.trim(),
+                release_date,
+                awards : +length,
+                rating : +length,
+                length : +length,
+                genre_id
+            })
+            .then(newMovie =>
+                console.log(newMovie));
+                return res.redirect('movies/deatil' + newMovie)
+        }
+    },
+    edit: function(req, res) {
+        // TODO
+    },
+    update: function (req,res) {
+        // TODO
+    },
+    delete: function (req, res) {
+        // TODO
+    },
+    destroy: function (req, res) {
+        // TODO
+    }
+
+}
+
+module.exports = moviesController;
